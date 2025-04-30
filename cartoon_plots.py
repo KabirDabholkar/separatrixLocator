45982a980bb6c19b8ca7563e2082487d56c5be5b
Kabir Dabholkar <kabirdabholkar@gmail.com>
2025-04-30 13:30:46 +0300

    making figures

--- cartoon_plots.py
+++ cartoon_plots.py
@@ -13,6 +13,31 @@ from functools import partial
 import operator
 colors = sns.color_palette("bright")
 
+
+plt.rcParams['text.usetex'] = True
+plt.rcParams["font.family"] = "sans-serif"
+plt.rcParams["mathtext.fontset"] = "dejavuserif"
+plt.rcParams['text.latex.preamble'] = r'\usepackage{amsmath}'
+
+def plot_arrows(ax, arrow_length = 0.5, arrow_start = [-1, -1.3], fontsize=12):
+    # Vertical arrow
+    ax.arrow(arrow_start[0], arrow_start[1], 0, arrow_length,
+             head_width=0.1, head_length=0.1, fc='k', ec='k', )
+    ax.text(arrow_start[0], arrow_start[1] + arrow_length + 0.13, 'input', 
+            ha='center', va='bottom', fontsize=fontsize)
+
+    # Diagonal arrow
+    ax.arrow(arrow_start[0], arrow_start[1], arrow_length / np.sqrt(2), arrow_length / np.sqrt(2),
+             head_width=0.1, head_length=0.1, fc='k', ec='k')
+    ax.text(arrow_start[0] + arrow_length / np.sqrt(2) + 0.1,
+            arrow_start[1] + arrow_length / np.sqrt(2) + 0.1,
+            'optimal', fontsize=fontsize)
+    # Horizontal arrow
+    ax.arrow(arrow_start[0], arrow_start[1], arrow_length, 0,
+             head_width=0.1, head_length=0.1, fc='k', ec='k')
+    ax.text(arrow_start[0] + arrow_length + 0.13, arrow_start[1], 'choice', 
+            ha='left', va='center', fontsize=fontsize)
+
 def remove_frame(ax):
     """
     Removes the frame and ticks from a matplotlib axis.
@@ -170,9 +195,28 @@ def cartoon_traj_and_inputs(dynamics_function,ax,T=20,steps=100):
         traj,
         odeint(lambda t, x: dynamics_function(x), traj[-1], time_points).detach().cpu()
     ])
-    ax.plot(traj[..., 0], traj[..., 1])
+    ax.arrow(traj[0, 0], traj[0, 1], traj[1, 0] - traj[0, 0], traj[1, 1] - traj[0, 1],
+                 head_width=0.1, head_length=0.1, fc='k', ec='k', length_includes_head=True)
+    ax.plot(traj[2:, 0], traj[2:, 1])
+    ax.arrow(-1, 0, 0.5, 0.5, head_width=0.1, head_length=0.1, fc='k', ec='k', length_includes_head=True)
+    # Add a small square to show 90 degree angle
+    # Add 90-degree angle marker at (-0.5, 0.5)
+    size = 0.15  # size of the angle marker
+    longer_edge = np.sqrt(2*size**2)
+    shorter_edge = np.sqrt(0.5*size**2)
+    corner = (-0.5-longer_edge, 0.5)
+    ax.plot(
+        [corner[0], corner[0] + shorter_edge],
+        [corner[1], corner[1] + shorter_edge],
+        color="k", linewidth=1, zorder=5,
+    )
+    ax.plot(
+        [corner[0], corner[0] + shorter_edge],
+        [corner[1], corner[1] - shorter_edge],
+        color="k", linewidth=1, zorder=5,
+    )
 
-def base_cartoon_plot(ax,x_limits=(-1.3,1.3),y_limits=(-1.3,1.3)):
+def base_cartoon_plot(ax,x_limits=(-1.3,1.3),y_limits=(-1.3,1.3),fontsize=12):
     # Check if the axis is 3D
     is_3d = hasattr(ax, 'zaxis')
     
@@ -228,6 +272,18 @@ def base_cartoon_plot(ax,x_limits=(-1.3,1.3),y_limits=(-1.3,1.3)):
         ax.plot(x, y, z, **plot_args)
     else:
         ax.plot(x, y, **plot_args)
+    # Add text label for the separatrix
+    if is_3d:
+        # For 3D plot, position text at midpoint of line
+        mid_idx = int(len(x) * 0.65)
+        ax.text(x[mid_idx], y[mid_idx], 0, 'separatrix',
+                ha='center', va='center', rotation=-45, color=colors[1], fontsize=fontsize)
+    else:
+        # For 2D plot, position text at midpoint of line
+        mid_idx = int(len(x) * 0.65)
+        ax.text(x[mid_idx], y[mid_idx]-0.08, 'separatrix',
+                ha='center', va='bottom', rotation=-45, color=colors[1], fontsize=fontsize)
+        
         
     ax.set_xlim(*x_limits)
     ax.set_ylim(*y_limits)
@@ -257,9 +313,10 @@ def plot_3d_cartoon(ax, x_limits=(-1.3,1.3), y_limits=(-1.3,1.3), z_limits=(0,1.
 
 if __name__ == '__main__':
     fig, ax = plt.subplots()
-    # plot_flow_streamlines(affine_bistable2D, ax, x_limits=(-1.3, 1.3), y_limits=(-1.3, 1.3), resolution=50,
-    #                       density=0.25, color='k', linewidth=0.5, alpha=0.4)
+    plot_flow_streamlines(affine_bistable2D, ax, x_limits=(-1.3, 1.3), y_limits=(-1.3, 1.3), resolution=50,
+                          density=0.4, color='skyblue', linewidth=0.5, alpha=0.4)
     base_cartoon_plot(ax)
+    plot_arrows(ax,arrow_length=0.3, arrow_start=[-1.1,-1])
     cartoon_traj_and_inputs(affine_bistable2D,ax)
     remove_frame(ax)
     # plt.show()
@@ -279,19 +336,22 @@ if __name__ == '__main__':
 
     # # Create 2D plot with kinetic energy contours
     # fig, ax = plt.subplots(figsize=(4,4))
-    # plot_kinetic_energy_contour(affine_bistable2D, ax, x_limits=(-2, 2), y_limits=(-2, 2), resolution=200, levels=15)
+    # kinetic_energy_function = dynamics_to_kinetic_energy(affine_bistable2D)
+    # X, Y, kinetic_energy_vals = evaluate_on_grid(kinetic_energy_function, x_limits=(-2, 2),y_limits=(-2, 2), resolution=200)
+    # ax.contourf(X, Y, np.log(kinetic_energy_vals+0.5), levels=15, cmap='Blues_r')
+    # # plot_kinetic_energy_contour(affine_bistable2D, ax, x_limits=(-2, 2), y_limits=(-2, 2), resolution=200, levels=15)
     # base_cartoon_plot(ax)
     # remove_frame(ax)
-    
+    #
     # # Save 2D figure
     # fig.savefig('plots_for_publication/cartoon_2d_kinetic_energy_contour.png', bbox_inches='tight', dpi=300)
-
-    # Create 2D plot with kinetic energy contours
-    fig, ax = plt.subplots(figsize=(4,4))
-    X,Y,separatrix_function = evaluate_on_grid(bistable_affine_separatrix_function, x_limits=(-2, 2), y_limits=(-2, 2), resolution=200)
-    ax.contourf(X, Y, (separatrix_function)**0.5, levels=15, cmap='Blues_r')
-    base_cartoon_plot(ax)
-    remove_frame(ax)
-    
-    # Save 2D figure
-    fig.savefig('plots_for_publication/cartoon_2d_separatrix_contour.png', bbox_inches='tight', dpi=300)
+    #
+    # # Create 2D plot with kinetic energy contours
+    # fig, ax = plt.subplots(figsize=(4,4))
+    # X,Y,separatrix_function = evaluate_on_grid(bistable_affine_separatrix_function, x_limits=(-2, 2), y_limits=(-2, 2), resolution=200)
+    # ax.contourf(X, Y, np.log(separatrix_function+0.25), levels=10, cmap='Blues_r')
+    # base_cartoon_plot(ax)
+    # remove_frame(ax)
+    #
+    # # Save 2D figure
+    # fig.savefig('plots_for_publication/cartoon_2d_separatrix_contour.png', bbox_inches='tight', dpi=300)
